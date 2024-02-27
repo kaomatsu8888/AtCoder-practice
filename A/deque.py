4a2cdd03c5b6230922d4674981ce9eb2d9083d17
kaomatsu8888 <m.kaoru0830@gmail.com>
2024-02-27 10:46:01 +0900

    commit

--- A/deque.py
+++ A/deque.py
@@ -4,16 +4,16 @@ t = 2 ... xを下に入れる
 t = 3 ... 上からx番目のカードを出力
 """
 
-Q = int(input())
-from collections import deque
+Q = int(input()) # Qを整数として取得
+from collections import deque # dequeモジュールをインポート/ dequeモジュールは双方向キューを提供する
 
-q = deque()
-for i in range(Q):
-    t, x = map(int, input().split())
+q = deque() # qを初期化
+for i in range(Q): # Qの数だけ繰り返す
+    t, x = map(int, input().split()) # t, xを整数として取得
     if t == 1:
-        q.appendleft(x)
+        q.appendleft(x) # xを左に追加
     elif t == 2:
-        q.append(x)
+        q.append(x) # xを追加
     elif t == 3:
-        print(q[x - 1])
+        print(q[x - 1]) # x番目の要素を出力
 
