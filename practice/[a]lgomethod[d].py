a4ddb52bf9909e10498ea9cf6398c9d17b244c5d
Kaoru Matsunaga <m.kaoru0830@gmail.com>
2024-03-08 23:29:30 +0900

    arugoshiki

---
+++ practice/[a]lgomethod[d].py
@@ -0,0 +1,5 @@
+S = "algorithm"
+print(S[3]) # 出力: "o"
+
+print(S[0:3]) # 出力: "alg"
+print(S[0]+S[1]+S[2]) # 出力: "alg"
---
+++ practice/number += 1.py
@@ -0,0 +1,16 @@
+deposit = 1500 # 貯金額
+deposit += 200
+deposit -= 100
+print(deposit) # 1600
+
+title = "Algo"
+title += title
+title += title
+print(title)  # AlgoAlgoAlgoAlgo
+
+num_1 = 5
+num_2 = 3
+num_1 += num_2
+num_1 *= num_2
+num_1 %= num_2
+print(num_1) # 0
