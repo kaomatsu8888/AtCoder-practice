7d2a935d1e260fd36e03481675d39d7558148be6
kaomatsu8888 <m.kaoru0830@gmail.com>
2024-03-26 19:32:34 +0900

    kentei

---
+++ Python3_kentei/ExamApp_middle/5.py
@@ -0,0 +1,41 @@
+"""次のコードを実行した結果として、正しいものはどれか。
+
+print(r'C:\sample\number')
+
+選択 1
+
+エラーになる。
+
+選択 2
+
+C:\sample
+umber
+
+選択 3
+
+C:samplenumber
+
+選択 4
+
+C:\sample\number
+"""
+"""次のコードを実行した結果として、正しいものはどれか。
+
+print(r'C:\sample\number')
+
+選択 1
+
+エラーになる。
+
+選択 2
+
+C:\sample
+umber
+
+選択 3
+
+C:samplenumber
+
+選択 4
+
+C:\sample\number"""
