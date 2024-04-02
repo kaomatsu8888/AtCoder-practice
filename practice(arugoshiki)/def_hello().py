99990485e73e5f45667121c331de6951bfad5a07
Kaoru Matsunaga <m.kaoru0830@gmail.com>
2024-04-02 22:55:01 +0900

    不要ファイル消去

--- practice(arugoshiki)/def_hello().py
+++
@@ -1,7 +0,0 @@
-# "Hello" と "World!" を交互に出力する関数
-def hello():
-    print("Hello!")
-    print("World!")
-
-for i in range(5):
-    hello()
--- practice(arugoshiki)/sunday,monday.py
+++
@@ -1,5 +0,0 @@
-DAY_OF_WEEKS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] # 0, 1, 2, 3, 4, 5, 6
-
-print(DAY_OF_WEEKS[0]) # Sunday
-print(DAY_OF_WEEKS[3]) # Wednesday
-print(DAY_OF_WEEKS[-1]) # Saturday
