72af09ae1ddbcbe1aa1355fa009ffa7082c07f1a
kaomatsu8888 <m.kaoru0830@gmail.com>
2024-04-07 20:07:11 +0900

    kentei

---
+++ u.py
@@ -0,0 +1,7 @@
+class Duck: # アヒルクラス
+    #def __init__(self): # コンストラクタ
+    def quack(self): # アヒルの鳴き声
+        self.birdsong = 'Quaaack!'
+
+    def sing(self): # アヒルの歌
+        self.bardsong() # バードソングを呼び出す
